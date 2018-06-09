a05c1132db1997381ef7c11a356f8cbdccbd64ab
vikas1062 <37344388+vikas1062@users.noreply.github.com>
2018-06-09 18:25:30 -0400

    Update DevApplicationTests.java

--- src/test/java/org/rahul/dev/DevApplicationTests.java
+++ src/test/java/org/rahul/dev/DevApplicationTests.java
@@ -7,7 +7,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class DevApplicationTests {
+p
+
+
 
 	@Test
 	public void contextLoads() {
